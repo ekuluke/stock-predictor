2609cf3f1b7ddea07c338ebeffeb98904f1a8e1c
ekuluke <luke.cowell@griffithuni.edu.au>
2020-12-10 02:20:08 -0800

    added wordlist in scrapers/ , specify a csv file from barcharts.com as the first argument when running stock_symbol_extractor.py

--- scrapers/twitter-scraper/twitter-scraper.py
+++ scrapers/twitter-scraper/twitter-scraper.py
@@ -1,1 +1,4 @@
 # Scrape
+
+
+pip install twitterscrapertwitterscraper "$TSLA OR Tesla from:wsj OR from:reuters OR from:business OR from:cnbc OR from:RANsquawk OR from:wsjmarkets" -o tsla_tweets.json -l 10000
