43cf03a6a74acba950805604916d8d8ba7768d11
ekuluke <luke.cowell@griffithuni.edu.au>
2020-12-14 03:02:55 -0800

    Added a name column to the stock wordlist as many twitter accounts do not use cash tags

--- scrapers/twitter-scraper/twitter-scraper.py
+++ scrapers/twitter-scraper/twitter-scraper.py
@@ -1,4 +1,12 @@
 # Scrape
 
+from twitterscraper import query_tweets
 
-pip install twitterscrapertwitterscraper "$TSLA OR Tesla from:wsj OR from:reuters OR from:business OR from:cnbc OR from:RANsquawk OR from:wsjmarkets" -o tsla_tweets.json -l 10000
+def load_stock_symbols():
+    symbols = []
+    f = open("../symbol-only-nasdaq-100-index-12-10-2020(1).csv", 'r')
+    for line in f:
+        symbols.append(line);
+    f.close()
+    return symbols;
+ "$TSLA OR Tesla from:wsj OR from:reuters OR from:business OR from:cnbc OR from:RANsquawk OR from:wsjmarkets" -o tsla_tweets.json -l 10000
