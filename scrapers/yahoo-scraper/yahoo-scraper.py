bc496f914f2beb03402a5f88967dca9f7036fe0f
conita <31991894+Conita02@users.noreply.github.com>
2021-01-13 16:19:15 +1000

    dataframes merged and csv stock data output to company folders

--- scrapers/yahoo-scraper/yahoo-scraper.py
+++ scrapers/yahoo-scraper/yahoo-scraper.py
@@ -3,6 +3,7 @@ import sys
 import os
 import yfinance as yf
 import pandas as pd
+from functools import reduce
 
 pd.set_option('max_columns', None)
 # pd.set_option('max_rows', None)
@@ -30,13 +31,20 @@ def readFile(path):
         return None
 
 def initStockData(stockName):
-    if not os.path.exists(os.cwd() + stockName):
+    if not os.path.exists(os.getcwd() +'/'+ stockName):
         try:
-            os.mkdir(os.cwd() + stockName)
+            os.mkdir(stockName)
         except OSError:
-            raise
+            raise OSError
         else:
             print("Created directory for " + stockName + " data")
+    else:
+        print("folder already exists!")
+
+
+def mergeDataFrames(dataFrames):
+    df_merged = reduce(lambda  left,right: pd.merge(left,right,on=['Date'], how='outer'), dataFrames)
+    return df_merged
 
 def stockData(stockName):
     try:
@@ -53,19 +61,7 @@ def stockData(stockName):
     low = hist["Low"]
     vol = hist["Volume"]
     dates = hist.index
-    print("----------------------------------------")
-    print(stockName + " Open Prices")
-    print("----------------------------------------")
-    print(hist["Open"])
-    print("----------------------------------------")
-    print(stockName + " Close Prices")
-    print("----------------------------------------")
-    print(hist["Close"])
-    print("----------------------------------------")
-    print(stockName + " Volumes")
-    print("----------------------------------------")
-    print(hist["Volume"])
-    print("----------------------------------------")
+
     return open, close, high, low, vol, dates
 
 def calculate_hlpct(dates, high, low):
@@ -93,28 +89,24 @@ if input.find("txt") == -1:
         open, close, high, low, vol, dates = stockData(input)
         pctchange = calculate_pctchange(dates, high, low)
         hlpct = calculate_hlpct(dates, high, low)
-        print(input + " hlpct")
-        print("----------------------")
-        print(hlpct)
-        print("----------------------")
-        print(input + " pctchange")
-        print("----------------------")
-        print(pctchange)
-        print("----------------------")
+        initStockData(input)
+        dataFrames = [open,close,high,low,vol,pctchange,hlpct]
+        dfMerged = mergeDataFrames(dataFrames)
+        dfMerged.to_csv(os.getcwd() + '/' + input + '/' + input+"StockData"+".csv")
     except:
-        print("not valid input exiting")
+        print("something went wrong exiting")
         sys.exit()
 else:
     data = readFile(input)
     for d in data:
-        open, close, high, low, vol, dates = stockData(d)
-        pctchange = calculate_pctchange(dates, high, low)
-        hlpct = calculate_hlpct(dates, high, low)
-        print(d + " hlpct")
-        print("--------------------")
-        print(hlpct)
-        print("--------------------")
-        print(d + " pctchange")
-        print("--------------------")
-        print(pctchange)
-        print("--------------------")
+        try:
+            open, close, high, low, vol, dates = stockData(d)
+            initStockData(d)
+            pctchange = calculate_pctchange(dates, high, low)
+            hlpct = calculate_hlpct(dates, high, low)
+            dataFrames = [open,close,high,low,vol,pctchange,hlpct]
+            dfMerged = mergeDataFrames(dataFrames)
+            dfMerged.to_csv(os.getcwd() + '/' + d + '/' + d + "StockData" + ".csv")
+        except:
+            raise Exception("something went wrong")
+            sys.exit()
